d79896abe4a6007b21e3ec4edfd21baa609f941c
e-sweeney <edwina.sweeney@atu.ie>
2026-04-21 15:25:37 +0000

    Added mlflow to training model artefact

--- trainBackup.py
+++ trainBackup.py
@@ -5,8 +5,6 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import r2_score, mean_squared_error
-
 
 
 
@@ -26,35 +24,20 @@ y = df["Result"]
 
 # Split data into training and testing sets (80% train, 20% test)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# Start MLflow Run
-with mlflow.start_run():
-    # Train Model
-    model = LinearRegression()
-    model.fit(X_train, y_train)
 
-    # Evaluate
-    predictions = model.predict(X_test)
-    r2 = r2_score(y_test, predictions)
-    mse = mean_squared_error(y_test, predictions)
-
-    # Log Metrics
-    mlflow.log_metric("r2_score", r2)
-    mlflow.log_metric("mse", mse)
-
-    # Log Model & Register
-    result = mlflow.sklearn.log_model(sk_model=model, artifact_path="model")
-    
-    mlflow.register_model(
-        model_uri=result.model_uri,
-        name="my-linear-regmodel"
-    )
-
-    print(f"Model logged with R2: {r2}")
+# Train Linear Regression Model
+model = LinearRegression()
+model.fit(X_train, y_train)
 
 # Save model to a .pkl file
 with open("model.pkl", "wb") as file:
     pickle.dump(model, file)
 
+result = mlflow.sklearn.log_model(model, "model")
 
+mlflow.register_model(
+    model_uri=result.model_uri,
+    name="my-linear-regmodel"
+)
 
 print("Model trained and saved as model.pkl!")
